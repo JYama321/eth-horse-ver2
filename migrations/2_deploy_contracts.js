f2eaf37765bbd7cbe096f90059d476a8d751f2f1
山本次郎 <yamamotojirou@yamamotojirounoMacBook-puro.local>
2018-07-06 17:08:08 +0900

    migration 追加

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+let HorseGameNew = artifacts.require("./HorseGame.sol");
+let GeneFunction = artifacts.require("./GeneFunction.sol");
+let RaceFunction = artifacts.require("./RaceFunction.sol");
+let Lottery = artifacts.require("./Lottery.sol");
+module.exports = function(deployer) {
+  deployer.deploy(GeneFunction).then(function () {
+    return deployer.deploy(RaceFunction)
+  }).then(function(){
+    return deployer.deploy(Lottery)
+  }).then(function () {
+    return deployer.deploy(HorseGameNew,GeneFunction.address,RaceFunction.address,Lottery.address,{gas: 5400000})
+  });
+};
