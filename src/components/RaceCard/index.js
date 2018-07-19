4d3c11b57fb3420347537014cfa6ac51ae17c05e
山本次郎 <yamamotojirou@yamamotojirounoMacBook-puro.local>
2018-07-19 22:55:36 +0900

    raceを繋いだ

--- src/components/RaceCard/index.js
+++ src/components/RaceCard/index.js
@@ -31,6 +31,7 @@ class RaceCard extends Component{
         super(props);
         this.state={
             isBookMakeModalOpen: false,
+            isShowRaceModalOpen: false,
             betEndTime: '',
             commitEndTime: '',
             betEndLoaded: false,
@@ -145,6 +146,25 @@ class RaceCard extends Component{
             return <button style={raceCardStyles.currentState} className={'race-current-state'}>Now Loading...</button>
         }
     }
+    openShowRaceModal(){
+        this.setState({
+            isShowRaceModalOpen: true
+        })
+    }
+    closeShowRaceModal(){
+        this.setState({
+            isShowRaceModalOpen: false
+        })
+    }
+    renderLink(state){
+        switch (state){
+            case 'ended':
+                return <button style={{textDecoration: 'underline', backgroundColor:'transparent', outline: 'none', border: 'none'}} onClick={()=>this.openShowRaceModal()}><p style={raceCardStyles.raceNameP}>{this.props.race[5]}</p></button>
+            default:
+                return <Link to={'/races/' + this.props.race[0].toNumber()}><p style={raceCardStyles.raceNameP}>{this.props.race[5]}</p></Link>;
+
+        }
+    }
     renderCurrentState(betEnd,commitEnd){
         const now = new Date(Date.now());
         if(commitEnd <= new Date(0) || betEnd <= new Date(0)){
@@ -176,11 +196,18 @@ class RaceCard extends Component{
             secretNum: e.target.value
         })
     }
+
     render(){
         const betEndTime = this.state.betEndRowDate;
         const commitEndTime = this.state.commitEndRowDate;
         const currentState = this.props.currentState === 'now betting' ? this.renderCurrentState(betEndTime,commitEndTime) : this.props.currentState;
         const raceId = this.props.raceId;
+        const horse1 = this.props.horseInfo.get(String(this.props.race[2].toNumber()));
+        const horse2 = this.props.horseInfo.get(String(this.props.race[3].toNumber()));
+        const horseGene1 = horse1 ? horse1[1].c.join(',').replace(/,/g,'') : '0000000';
+        const horseGene2 = horse2 ? horse2[1].c.join(',').replace(/,/g,'') : '000000';
+        const winnerHorseIndex = this.props.race[2].toNumber() === this.props.race[8].toNumber() ? '0' : '1';
+        const winnerHorseName = this.props.horseInfo.get(String(this.props.race[8].toNumber())) ? this.props.horseInfo.get(String(this.props.race[8].toNumber()))[0] : '';
         if(this.props.isMyRace){
             return(
                 <div style={raceCardStyles.cardContainer}>
@@ -190,24 +217,46 @@ class RaceCard extends Component{
                         race={this.props.race}
                         horseInfo={this.props.horseInfo}
                     />
+                    <Modal
+                        isOpen={this.state.isShowRaceModalOpen}
+                        style={raceCardStyles.raceResultModal}
+                        onRequestClose={()=>this.closeShowRaceModal()}
+                    >
+                        <div style={raceCardStyles.raceResultHeader}>
+                            check race result
+                        </div>
+                        <div style={raceCardStyles.raceResultContent}>
+                            <div style={raceCardStyles}>
+                                <a href={'http://ehth-horse-scenes.s3-website-ap-northeast-1.amazonaws.com/StartScenes/StartScene.html?' +
+                                'tex1=' + horseGene1.slice(horseGene1.length-38,horseGene1.length-20) +
+                                '&tex2=' + horseGene2.slice(horseGene1.length-38,horseGene1.length-20) +
+                                '&winnerIndex=' + winnerHorseIndex +
+                                '&winnerHorseName=' + winnerHorseName} target="_blank">See Race Movie</a>
+                            </div>
+                            or
+                            <div>
+                                <Link to={'/races/' + this.props.race[0].toNumber()}>Just check result</Link>
+                            </div>
+                        </div>
+                    </Modal>
                     <div style={raceCardStyles.cartContainerTop}>
                         <p>{this.renderTimeInfo(betEndTime)}</p>
                     </div>
                     <div style={raceCardStyles.raceInfoContainer}>
-                        <Link to={'/races/' + this.props.race[0].toNumber()}><p style={raceCardStyles.raceNameP}>{this.props.race[5]}</p></Link>
+                        {this.renderLink(currentState)}
                         <p style={raceCardStyles.winnerPrizeP}>Winner Prize:&nbsp; <b>{window.web3.fromWei(this.props.race[6],'ether').toNumber().toFixed(2)}</b> ETH + <b>{this.props.race[7].toNumber()}</b> % of total bet</p>
                         {this.renderCurrentStateButton(currentState)}
                     </div>
                     <div style={raceCardStyles.horseImgContainer}>
                         <div style={raceCardStyles.horseContainer}>
                             <div className='horse-back' style={raceCardStyles.horseBack}>
-                                <HorseImage type={'normal'} horseGene={this.props.horseInfo.get(String(this.props.race[2].toNumber())) ? this.props.horseInfo.get(String(this.props.race[2].toNumber()))[1].c.join(',').replace(/,/g,'') : '0000000'}/>
+                                <HorseImage type={'normal'} horseGene={horseGene1}/>
                             </div>
                             <p style={raceCardStyles.horseNameP}>{this.props.horseInfo.get(String(this.props.race[2].toNumber())) ? this.props.horseInfo.get(String(this.props.race[2].toNumber()))[2] : '???'}</p>
                         </div>
                         <div style={raceCardStyles.horseContainer}>
                             <div className='horse-back' style={raceCardStyles.horseBack}>
-                                <HorseImage type={'normal'} horseGene={this.props.horseInfo.get(String(this.props.race[3].toNumber())) ? this.props.horseInfo.get(String(this.props.race[3].toNumber()))[1].c.join(',').replace(/,/g,'') : '000000'}/>
+                                <HorseImage type={'normal'} horseGene={horseGene2}/>
                             </div>
                             <p style={raceCardStyles.horseNameP}>{this.props.horseInfo.get(String(this.props.race[3].toNumber())) ? this.props.horseInfo.get(String(this.props.race[3].toNumber()))[2] : '???'}</p>
                         </div>
@@ -243,11 +292,36 @@ class RaceCard extends Component{
                             >Confirm</Button>
                         </div>
                     </Modal>
+                    <Modal
+                        isOpen={this.state.isShowRaceModalOpen}
+                        style={raceCardStyles.raceResultModal}
+                        onRequestClose={()=>this.closeShowRaceModal()}
+                    >
+                        <div style={raceCardStyles.raceResultHeader}>
+                            check race result
+                        </div>
+                        <div style={raceCardStyles.raceResultContent}>
+                            <div style={raceCardStyles.raceResultModalButton}>
+                                <a style={{color: 'white'}} href={'http://ehth-horse-scenes.s3-website-ap-northeast-1.amazonaws.com/StartScenes/StartScene.html?' +
+                                'tex1=' + horseGene1.slice(horseGene1.length-38,horseGene1.length-20) +
+                                '&tex2=' + horseGene2.slice(horseGene1.length-38,horseGene1.length-20) +
+                                '&winnerIndex=' + winnerHorseIndex +
+                                '&winnerHorseName=' + winnerHorseName} target="_blank">See Race Movie
+                                </a>
+                            </div>
+                            or
+                            <div style={raceCardStyles.raceResultModalButtonBlue} className='just-check-result'>
+                                <Link to={'/races/' + this.props.race[0].toNumber()}>
+                                    Just check result
+                                </Link>
+                            </div>
+                        </div>
+                    </Modal>
                     <div style={raceCardStyles.cartContainerTop}>
                         <p>{this.renderTimeInfo(betEndTime)}</p>
                     </div>
                     <div style={raceCardStyles.raceInfoContainer}>
-                        <Link to={'/races/' + this.props.race[0].toNumber()}><p style={raceCardStyles.raceNameP}>{this.props.race[5]}</p></Link>
+                        {this.renderLink(currentState)}
                         <p style={raceCardStyles.winnerPrizeP}>Winner Prize:&nbsp;<b>{window.web3.fromWei(this.props.race[6],'ether').toNumber().toFixed(2)}</b> ETH + <b>{this.props.race[7].toNumber()}</b> % of total bet</p>
                         {this.renderCurrentStateButton(currentState)}
                     </div>
--- src/components/RaceCard/styles.js
+++ src/components/RaceCard/styles.js
@@ -93,6 +93,56 @@ export const raceCardStyles = {
             zIndex: 8888
         }
     },
+    raceResultModal: {
+        content: {
+            top : '50%',
+            left  : '50%',
+            right : 'auto',
+            bottom : 'auto',
+            marginRight : '-50%',
+            transform : 'translate(-50%, -50%)',
+            height: '200px',
+            width: '450px',
+            position: 'absolute',
+            padding: 0,
+            zIndex: 8888
+        }
+    },
+    raceResultHeader: {
+        height: '80px',
+        width: '100%',
+        color: 'white',
+        backgroundColor: 'black',
+        lineHeight: '80px',
+        textAlign: 'center'
+    },
+    raceResultContent: {
+        width: '90%',
+        height: '120px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        paddingTop: '45px',
+        position: 'relative',
+        margin: '0 auto'
+    },
+    raceResultModalButton: {
+        width: '150px',
+        height: '30px',
+        borderRadius: '15px',
+        backgroundColor: 'rgba(240,30,30,0.8)',
+        color: 'white',
+        lineHeight: '30px',
+        textAlign: 'center'
+    },
+    raceResultModalButtonBlue: {
+        width: '150px',
+        height: '30px',
+        borderRadius: '15px',
+        backgroundColor: 'rgba(30,30,240,0.8)',
+        color: 'white',
+        lineHeight: '30px',
+        textAlign: 'center'
+    },
     modalTitle: {
         height: '80px',
         width: '100%',
