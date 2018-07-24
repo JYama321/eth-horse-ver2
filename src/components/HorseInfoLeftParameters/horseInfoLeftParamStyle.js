9f537155704b44b6259572af1eeec8e49e1f8b95
山本次郎 <yamamotojirou@yamamotojirounoMacBook-Air.local>
2018-07-25 08:54:09 +0900

    レイアウトを整えた

--- src/components/HorseInfoLeftParameters/horseInfoLeftParamStyle.js
+++ src/components/HorseInfoLeftParameters/horseInfoLeftParamStyle.js
@@ -1,52 +1,55 @@
 export const hILeftParamStyle = {
-  container: {
-    width: '100%',
-    height: '100%',
-    position: 'relative'
-  },
-  placeText: {
-    width: '100%',
-    height: '30px',
-    textAlign: 'left',
-    lineHeight: '30px',
-    fontSize: '15px'
-  },
-  powerTotalTex: {
-    width: '100%',
-    height: '75px',
-    lineHeight: '75px',
-    fontSize: '21px',
-    textAlign: 'left'
-  },
-  horseParamDiagram: {
-    height: '30px',
-    width: '100%',
-    position: 'relative',
-    marginTop: '14px'
-  },
-  strengthParam: (status,backColorAlpha) => ({
-    height: '100%',
-    float: 'left',
-    backgroundColor: `rgba(0,0,0,${backColorAlpha})`,
-    width: Math.ceil(status / 1000 * 20)+"%"
-  }),
-  statusParamDiagram: {
-    width: '300px',
-    height: '100px',
-    marginTop: '30px',
-    paddingBottom: '56px',
-    borderBottom: '2px solid rgba(0,0,0,0.5)',
-    position: 'relative'
-  },
-  sireAndRaceNum: {
-    width: '300px',
-    height: '30px',
-    position: 'relative'
-  },
-  sireRaceNumP: {
-    lineHeight: '15px',
-    height: '15px',
-    textAlign: 'left',
-    fontSize: '15px'
-  }
+    container: {
+        width: '100%',
+        height: '100%',
+        position: 'relative'
+    },
+    placeText: {
+        width: '300px',
+        height: '25px',
+        textAlign: 'left',
+        lineHeight: '25px',
+        fontSize: '15px',
+        backgroundColor: 'black',
+        color: 'white',
+        paddingLeft: '10px'
+    },
+    powerTotalTex: {
+        width: '100%',
+        height: '75px',
+        lineHeight: '75px',
+        fontSize: '21px',
+        textAlign: 'left'
+    },
+    horseParamDiagram: {
+        height: '30px',
+        width: '100%',
+        position: 'relative',
+        marginTop: '14px'
+    },
+    strengthParam: (status,backColorAlpha) => ({
+        height: '100%',
+        float: 'left',
+        backgroundColor: `rgba(0,0,0,${backColorAlpha})`,
+        width: Math.ceil(status / 1000 * 20)+"%"
+    }),
+    statusParamDiagram: {
+        width: '300px',
+        height: '100px',
+        marginTop: '30px',
+        paddingBottom: '56px',
+        borderBottom: '2px solid rgba(0,0,0,0.5)',
+        position: 'relative'
+    },
+    sireAndRaceNum: {
+        width: '300px',
+        height: '30px',
+        position: 'relative'
+    },
+    sireRaceNumP: {
+        lineHeight: '15px',
+        height: '15px',
+        textAlign: 'left',
+        fontSize: '15px'
+    }
 };
